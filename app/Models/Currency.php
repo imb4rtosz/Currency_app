f2b78133ff7bd95641b1ee4d9c14b5b876a39b98
Bartosz <84rt3kindeed@gmail.com>
2022-01-03 11:39:26 +0100

    konfiguracja uuid

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Currency extends Model
-{
+{   
+    use Uuids;
     use HasFactory;
 }
